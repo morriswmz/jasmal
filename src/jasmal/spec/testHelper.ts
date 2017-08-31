4280fa13c1757fdf718c2d0c430dc88a77c1bc39
morriswmz <morriswmz@gmail.com>
2017-08-31 10:43:41 -0500

    checkTensor() now accepts an array of tolerance factors. Added a new test helper.

--- src/jasmal/spec/testHelper.ts
+++ src/jasmal/spec/testHelper.ts
@@ -2,6 +2,7 @@ import { ComplexNumber } from '../lib/complexNumber';
 import { Tensor } from '../lib/tensor';
 import { ShapeHelper } from '../lib/helper/shapeHelper';
 import { DTypeHelper } from '../lib/dtype';
+import { OpInput, OpOutput, Scalar } from '../lib/commonTypes';
 
 function areCloseByAbsoluteValue(actual: number, expected: number, tol: number): boolean {
     return Math.abs(actual - expected) <= tol;
@@ -29,7 +30,7 @@ export function maxAbs(x: ArrayLike<number>): number {
 }
 
 export function checkNumber(actual: any, expected: number, tolerance: number = 0,
-                            absolute: boolean = false, msgPrefix: string = ''): void {
+                            absolute: boolean = true, msgPrefix: string = ''): void {
     if (actual === expect) {
         return;
     }
@@ -81,7 +82,7 @@ export function checkArrayLike<T>(actual: ArrayLike<T>, expected: ArrayLike<T>):
 };
 
 export function checkComplex(actual: any, expected: ComplexNumber, tolerance: number = 0,
-                             absolute: boolean = false, msgPrefix: string = ''): void {
+                             absolute: boolean = true, msgPrefix: string = ''): void {
     if (actual === expected) return;
     if (!(actual instanceof ComplexNumber)) {
         throw new Error(`${msgPrefix}Expecting a complex number.`);
@@ -90,7 +91,7 @@ export function checkComplex(actual: any, expected: ComplexNumber, tolerance: nu
     checkNumber(actual.im, expected.im, tolerance, absolute, 'Imaginary part: ');
 }
 
-export function checkTensor(actual: any, expected: Tensor, tolerance: number = 0, absolute: boolean = true): void {
+export function checkTensor(actual: any, expected: Tensor, tolerance: number | ArrayLike<number> = 0, absolute: boolean = true): void {
     if (actual === expected) return;
     if (!(actual instanceof Tensor)) {
         fail(`Expecting a tensor, but got "${Object.prototype.toString.call(actual)}".`);
@@ -114,19 +115,78 @@ export function checkTensor(actual: any, expected: Tensor, tolerance: number = 0
         fail('Expecting a tensor without complex storage, got a tensor with one.');
         return;
     }
+    let isComplex = expected.hasComplexStorage();
     let reActual = actual.realData;
     let reExpected = expected.realData;
-    if (expected.hasComplexStorage()) {
-        let imActual = actual.imagData;
-        let imExpected = expected.imagData;
-        for (let i = 0;i < reActual.length;i++) {
-            checkComplex(new ComplexNumber(reActual[i], imActual[i]),
-                new ComplexNumber(reExpected[i], imExpected[i]),
-                tolerance, absolute, `Index ${i}: `);
+    let imActual = isComplex ? actual.imagData : [];
+    let imExpected = isComplex ? expected.imagData : [];
+    if (typeof tolerance === 'number') {
+        if (isComplex) {
+            for (let i = 0;i < reActual.length;i++) {
+                checkComplex(new ComplexNumber(reActual[i], imActual[i]),
+                    new ComplexNumber(reExpected[i], imExpected[i]),
+                    tolerance, absolute, `Index ${i}: `);
+            }
+        } else {
+            for (let i = 0;i < reActual.length;i++) {
+                checkNumber(reActual[i], reExpected[i], tolerance, absolute, `Index ${i}: `);
+            }
         }
     } else {
-        for (let i = 0;i < reActual.length;i++) {
-            checkNumber(reActual[i], reExpected[i], tolerance, absolute, `Index ${i}: `);
+        if (tolerance.length !== reExpected.length) {
+            throw new Error('The length of tolerance must match the size of the tensor.');
+        }
+        if (isComplex) {
+            for (let i = 0;i < reActual.length;i++) {
+                checkComplex(new ComplexNumber(reActual[i], imActual[i]),
+                    new ComplexNumber(reExpected[i], imExpected[i]),
+                    tolerance[i], absolute, `Index ${i}: `);
+            }
+        } else {
+            for (let i = 0;i < reActual.length;i++) {
+                checkNumber(reActual[i], reExpected[i], tolerance[i], absolute, `Index ${i}: `);
+            }
         }
     }
+}
+
+export function testUnaryOpInBatch(fn: (x: OpInput) => OpOutput, data: Array<[Scalar, Scalar, number]>, absolute: boolean = true) {
+    let n = data.length;
+    let reExpected = new Array(n);
+    let imExpected: number[] = [];
+    let reInput = new Array(n);
+    let imInput: number[] = [];
+    let tolArr: number[] = new Array(n);
+    for (let i = 0;i < n;i++) {
+        let input = data[i][0];
+        let expected = data[i][1];
+        if (input instanceof ComplexNumber) {
+            if (imInput.length === 0) {
+                imInput.length = n;
+                for (let k = 0;k < n;k++) {
+                    imInput[k] = 0;
+                }
+            }
+            reInput[i] = input.re;
+            imInput[i] = input.im;
+        } else {
+            reInput[i] = input;
+        }
+        if (expected instanceof ComplexNumber) {
+            if (imExpected.length === 0) {
+                imExpected.length = n;
+                for (let k = 0;k < n;k++) {
+                    imExpected[k] = 0;
+                }
+            }
+            reExpected[i] = expected.re;
+            imExpected[i] = expected.im;
+        } else {
+            reExpected[i] = expected;
+        }
+        tolArr[i] = data[i][2];
+    }
+    let actual = fn(Tensor.fromArray(reInput, imInput));
+    let expected = Tensor.fromArray(reExpected, imExpected);
+    checkTensor(actual, expected, tolArr, absolute);
 }
